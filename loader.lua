71b4f7ecab5a24dc4147b6ccb33f364df70502ed
pathetic bitch <vewarisking575@gmail.com>
2026-09-25 02:30:44 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,3 @@
 -- rise [https://discord.gg/HEhYHVJ2X6]
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/b7cca66d493ea7fe18629f1b27a3ecc5.lua"))()
+--loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/b7cca66d493ea7fe18629f1b27a3ecc5.lua"))()
+loadstring(game:HttpGet("https://files.suyo.my.id/assets/raw/risedihhh.txt"))()
